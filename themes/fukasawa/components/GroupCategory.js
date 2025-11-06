d0c869b411017d07279b81a856060e6849539654
XiaoFeng9009 <xiaofenglu894@gmail.com>
2025-11-06 15:44:02 +0800

    给Category增加折叠组件

--- themes/fukasawa/components/GroupCategory.js
+++ themes/fukasawa/components/GroupCategory.js
@@ -6,24 +6,34 @@ function GroupCategory ({ currentCategory, categories }) {
   }
 
   return <>
-    <div id='category-list' className='dark:border-gray-600 flex flex-wrap'>
-      {categories.map(category => {
-        const selected = currentCategory === category.name
-        return (
-          <SmartLink
-            key={category.name}
-            href={`/category/${category.name}`}
-            passHref
-            className={(selected
-              ? 'hover:text-white dark:hover:text-white bg-gray-600 text-white '
-              : 'dark:text-gray-400 text-gray-500 hover:text-white hover:bg-gray-500 dark:hover:text-white') +
-              '  text-sm w-full items-center duration-300 px-2  cursor-pointer py-1 font-light'}>
-
-            <i className={`${selected ? 'text-white fa-folder-open' : 'fa-folder text-gray-400'} fas mr-2`} />{category.name}({category.count})
-          </SmartLink>
-        )
-      })}
-    </div>
+    <details className="group cursor-pointer">
+      <summary className="list-none py-2 px-2 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 font-medium flex items-center">
+        <i className="fas fa-chevron-right mr-2 group-open:rotate-90 transition-transform duration-200 text-xs" />
+        <span>工作室或设计师列表</span>
+        <span className="ml-2 text-xs bg-gray-200 dark:bg-gray-700 rounded-full px-2 py-0.5">
+          {categories.length}
+        </span>
+      </summary>
+    
+    	<div id='category-list' className='dark:border-gray-600 flex flex-wrap'>
+        {categories.map(category => {
+          const selected = currentCategory === category.name
+          return (
+            <SmartLink
+              key={category.name}
+              href={`/category/${category.name}`}
+              passHref
+              className={(selected
+                ? 'hover:text-white dark:hover:text-white bg-gray-600 text-white '
+                : 'dark:text-gray-400 text-gray-500 hover:text-white hover:bg-gray-500 dark:hover:text-white') +
+                '  text-sm w-full items-center duration-300 px-2  cursor-pointer py-1 font-light'}>
+  
+              <i className={`${selected ? 'text-white fa-folder-open' : 'fa-folder text-gray-400'} fas mr-2`} />{category.name}({category.count})
+            </SmartLink>
+          )
+        })}
+      </div>
+    </details>
   </>
 }
 
